1d00e5a2c723f41a39fd356de6782127e2139979
Nguyễn Đình Huy <151980955+huynguyendinhhcmut@users.noreply.github.com>
2024-05-08 22:45:15 +0700

    Update wrapper_lab3.sv

--- wrapper_lab3.sv
+++ wrapper_lab3.sv
@@ -9,7 +9,7 @@ normalized normalized (
 	.Cin(SW[8]), .clk50M(CLOCK_50), .rst(KEY[0]), 
 	.en_A(KEY[1]), .en_B(KEY[2]), .en_cin(KEY[3]), 
 	.sign(SW[7]), .Exp(SW[6:4]), .Fract(SW[3:0]), 
-	.Result_fract(LEDR[3:0]), .Result_exp(LEDR[6:4])
+	.Result_fract(LEDR[3:0]), .Result_exp(LEDR[6:4]), .result_sign(LEDR[7])
 );
 
 endmodule*/
